9dc05d47da95514aa23ca7be77303a83a3d8425a
ClaraShar <179800587@qq.com>
2019-03-12 19:07:00 +0800

    2019-3-12

---
+++ 小试牛刀/数位之和.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin>>s;
+	int res=0;
+	for(int i=0;i<s.length();++i)
+		res+=s[i]-'0';
+	cout<<res;
+	cin>>s;
+	return 0;
+}
